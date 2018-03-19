b9c610e3b5964befddf62b81b66982bf708f8ec0
Shweta Sharma <shweta@edwisor.com>
2018-03-19 15:23:08 +0530

    blog app

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
 var shortid = require('shortid');
 const fs = require('fs');
 var cresponse = {
